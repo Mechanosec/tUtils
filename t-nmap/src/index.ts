c922220568ea986fb74c04dd31bab4724abedc5b
O.Krasovskyi <mechanosec30@gmail.com>
2026-04-26 20:13:37 +0300

    feat(lazynmap): add alternate screen buffer to entry point

--- t-nmap/src/index.ts
+++ t-nmap/src/index.ts
@@ -3,4 +3,13 @@ import React from 'react';
 import { render } from 'ink';
 import { App } from './App.js';
 
-render(React.createElement(App));
+process.stdout.write('\x1b[?1049h');
+process.stdout.write('\x1b[H');
+
+const cleanup = () => { process.stdout.write('\x1b[?1049l'); };
+process.on('exit', cleanup);
+process.on('SIGINT', () => { cleanup(); process.exit(0); });
+process.on('SIGTERM', () => { cleanup(); process.exit(0); });
+
+const { waitUntilExit } = render(React.createElement(App));
+waitUntilExit().then(() => { cleanup(); process.exit(0); });
